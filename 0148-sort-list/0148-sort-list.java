49960bc03d9e5b09fdcdfa0455e561ebda9eb14a
deepak-kumar9308 <155214667+deepak-kumar9308@users.noreply.github.com>
2026-08-19 10:32:37 +0530

    Time: 8 ms (99.7%), Space: 59.3 MB (77.24%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    // Merge two sorted linked lists
+    public ListNode merge(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        // Attach remaining nodes
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    // Sort linked list
+    public ListNode sortList(ListNode head) {
+
+        // Base case
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Find middle
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Split into two halves
+        ListNode secondHalf = slow.next;
+        slow.next = null;
+
+        // Recursively sort both halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(secondHalf);
+
+        // Merge both sorted halves
+        return merge(left, right);
+    }
+}
